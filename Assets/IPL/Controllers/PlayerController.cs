9cb0b6978c1fd60267163f2f7c88d0ab6f29c91b
Jean-Gobert de Coster <jeangodecoster@outlook.com>
2020-10-07 17:00:09 +0200

    Inital commit

---
+++ Assets/IPL/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //déplacement droite-gauche
+        //inverser le sprite quand on change de direction
+        //sauter mais seulement quand on est au sol
+    }
+}
